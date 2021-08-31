15be8c2a4055d2876b49de2d88fdc4da1d5bc82c
Ihsan <78595134+UserLazy@users.noreply.github.com>
2021-09-01 04:53:53 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/subinps/MusicPlayer.git /MusicPlayer
-cd /MusicPlayer
+git clone https://UserLazy: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@github.com/UserLazy/Musicoda /Musicoda
+cd /Musicoda
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
